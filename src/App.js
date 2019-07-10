06cf5d208772548991bc99b91cc088a58f5a5478
Andrea <andrea.belluccia@gmail.com>
2019-07-10 23:11:48 +0200

    Initial commit of components without tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Header from "./components/header";
+import "./app.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Ciao</p>
+        <Header />
       </div>
     );
   }
